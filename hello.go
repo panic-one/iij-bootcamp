6519c241adebaa08d09be74553aef9317b993bd6
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-15 22:00:28 +0900

    4.2.1

--- hello.go
+++ hello.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, W0rld!!")
+	var Watashi_no_Hensu string = "GYUDON"
+	fmt.Println(Watashi_no_Hensu)
+	main2()
+}
+
+func Eat(name string) bool {
+	if name == "" {
+		return false
+	} else {
+		fmt.Println(name)
+		return true
+	}
+}
+
+func main2() {
+	var name1 string = "GYUDON"
+	if ok := Eat(name1); !ok {
+		fmt.Println("cannot eat: ", name1)
+	}
+
+	var name2 string = ""
+	if ok :=Eat(name2); !ok {
+		fmt.Println("cannot eat: ", name2)
+	}
 }

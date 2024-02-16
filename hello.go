eebc91515e02f6899c4d21ea4df039baa789ba37
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-16 18:28:06 +0900

    5.1.1

--- hello.go
+++ hello.go
@@ -1,46 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-
-func Eat(name string) bool {
+func Eat(name string) (bool, error) {
 	if name == "" {
-		return false
-	} else {
-		fmt.Println(name)
-		return true
-	}
-}
-
-func main() {
-	var name1 string = "GYUDON"
-	if ok := Eat(name1); !ok {
-		fmt.Println("cannt eat: ", name1)
-	}
-
-	var name2 string = ""
-	if ok := Eat(name2); !ok {
-		fmt.Println("cannt eat: ", name2)
-		main2()
-}
-}
-
-func Eat2(name string) (bool, error) {
-	if name == "" {
-		return false, fmt.Errorf("name is empty.")
+		return false, fmt.Errorf("name is empty")
 	}
 	fmt.Println(name)
 	return true, nil
 }
 
-func main2() {
+func main() {
 	var name1 string = "GYUDON"
-	if _, err := Eat2(name1); err != nil {
-		fmt.Println("cannot eat: ", err)
+	if _, err := Eat(name1); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err)
 	}
 
 	var name2 string = ""
-	if _, err := Eat2(name2); err != nil {
-		fmt.Println("cannot eat: ", err)
+	if _, err := Eat(name2); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err)
 	}
 }

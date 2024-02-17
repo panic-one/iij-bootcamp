3185e07821deb23380b34adebd77efcc4068d05c
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-17 20:45:12 +0900

    4.2.2

--- hello.go
+++ hello.go
@@ -3,29 +3,7 @@ package main
 import "fmt"
 
 
-func Eat(name string) bool {
-	if name == "" {
-		return false
-	} else {
-		fmt.Println(name)
-		return true
-	}
-}
-
-func main() {
-	var name1 string = "GYUDON"
-	if ok := Eat(name1); !ok {
-		fmt.Println("cannt eat: ", name1)
-	}
-
-	var name2 string = ""
-	if ok := Eat(name2); !ok {
-		fmt.Println("cannt eat: ", name2)
-		main2()
-}
-}
-
-func Eat2(name string) (bool, error) {
+func Eat(name string) (bool, error) {
 	if name == "" {
 		return false, fmt.Errorf("name is empty.")
 	}
@@ -33,7 +11,7 @@ func Eat2(name string) (bool, error) {
 	return true, nil
 }
 
-func main2() {
+func main() {
 	var name1 string = "GYUDON"
 	if _, err := Eat2(name1); err != nil {
 		fmt.Println("cannot eat: ", err)

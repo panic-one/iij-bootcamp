30aa83a744b466266ad47d8023b286cdd07163c8
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-18 16:44:20 +0900

    5.2.1

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
-package main
+package 
+	shop
 
 import (
 	"fmt"
 	"os"
+	"./shop"
 )
 
 func Eat(name string) (bool, error) {
@@ -15,12 +17,12 @@ func Eat(name string) (bool, error) {
 
 func main() {
 	var name1 string = "GYUDON"
-	if _, err := Eat(name1); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err)
+	if _, err := shop.Eat(name1); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
 	}
 
 	var name2 string = ""
-	if _, err := Eat(name2); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n", err)
+	if _, err := shop.Eat(name2); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
 	}
 }

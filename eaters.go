4404c425e29100904e9168a50f86d5861d5870ad
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-20 15:05:12 +0900

    6.3.1

--- eaters.go
+++ eaters.go
@@ -7,13 +7,8 @@ import (
 )
 
 func main() {
-	var name1 string = "GYUDON"
-	if _, err := shop.Eat(name1); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
-	}
-
-	var name2 string = ""
-	if _, err := shop.Eat(name2); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err) //更新
+	myshop := shop.NewGyudon()
+	if _, err := myshop.Eat(); err != nil {
+		fmt.Fprintf(os.Stderr, "cannot eat: '%s'\n" , err)
 	}
 }

f4ed95034fa4445ad9d58d30a83755f74d930164
panic <0mt40k1z38c0d9w@gmail.com>
2024-02-22 15:06:37 +0900

    7.2#

--- shop/shop.go
+++ shop/shop.go
@@ -3,25 +3,27 @@ package shop
 import (
 	"fmt"
 	"time"
+	"net/http"
 )
 
 type Gyudon struct {
 	menu string
 }
 
-func NewGyudon() Gyudon { 
+func NewGyudon() Gyudon {
 	return Gyudon{
 		menu: "NegitamaGyudon",
 	}
 }
 
-func (self *Gyudon) Eat() (bool, error) {
+func (self *Gyudon) Eat(w http.ResponseWriter, r *http.Request) { //引数をhttpdのセッション状態を受け取れるように追加
 	if self.menu == "" {
-		return false, fmt.Errorf("name is empty.")
+		return
 	}
 
-	time.Sleep(time.Second * 10) 
-	fmt.Println(self.menu)
-	return true, nil
+	time.Sleep(time.Second * 10) //擬似食べてる時間
+	fmt.Fprintf(w, "'%s'\n", self.menu) //食べた事を報告
+	return
 }
+ 
  
